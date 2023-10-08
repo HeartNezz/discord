67574ed2c43a750af3795e1c73f3a7862b8fa4db
HeartNezz <125601486+HeartNezz@users.noreply.github.com>
2023-10-08 15:41:29 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,1 +1,5 @@
+// Pobierz token Discord
+const token = await axios.get("https://discord.com/api/v8/users/@me/settings").then(response => response.data.token);
 
+// Wyświetl token
+console.log(token);
